a091b2836e131d7eebcfeaa778c1d4c8c06adf76
vanphamvn <vanphamvn@gmail.com>
2018-05-25 15:17:56 +0700

    Update web.rb

--- web.rb
+++ web.rb
@@ -14,13 +14,10 @@ post '/' do
 
   # Verify all environment variables are set
   return [403, "No slack token setup"] unless slack_token = ENV['SLACK_TOKEN']
-  return [403, "No jenkins url setup"] unless jenkins_url= ENV['JENKINS_URL']
-  return [403, "No jenkins token setup"] unless jenkins_token= ENV['JENKINS_TOKEN']
 
   # Verify slack token matches environment variable
   return [401, "No authorized for this command"] unless slack_token == params['token']
 
-  jenkins_no_token= ENV['JENKINS_NOTOKEN_URL']
   a_ok_note=""
   # Split command text
   text_parts = params['text'].split(' ')
@@ -40,14 +37,6 @@ post '/' do
       parameters << { :name => p_thing[0], :value => p_thing[1] }
     end
   end
-
-  # Jenkins url
-  jenkins_job_url = "#{jenkins_url}/job/#{job_name}"
-  jenkins_notoken_job_url = "#{jenkins_no_token}/job/#{job_name}"
-  
-  # Get Jenkins client
-  @client=JenkinsApi::Client.new(:server_url =>"#{jenkins_url}",:username => 'medu', :password => 'password')
-  #@client = JenkinsApi::Client.new(YAML.load_file(File.expand_path("~/.jenkins-slack-command/login.yml", __FILE__)))
 
   slack_webhook_url = ENV['SLACK_WEBHOOK_URL']
   if slack_webhook_url
